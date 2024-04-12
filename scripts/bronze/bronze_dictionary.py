43f2719fb3e6f4769df49c35a886ff51d8a9751c
sauravsagarST <saurav.sagar@symphonytalent.com>
2024-04-12 12:04:05 +0530

    adding userInsight test tables

--- scripts/bronze/bronze_dictionary.py
+++ scripts/bronze/bronze_dictionary.py
@@ -10,3 +10,23 @@ bronze_folder_dict = {
     'duplicate_tc': ['folder_id'],
     'null_tc': ['folder_id','users_id','hierarchy_id']
 }
+bronze_users_dict = {
+    'duplicate_tc': ['users_id'],
+    'null_tc': ['users_id']
+}
+bronze_user_logins_dict ={
+    'duplicate_tc': ['users_id'],
+    'null_tc': ['users_id']
+}
+bronze_folder_candidates_dict = {
+    'duplicate_tc': ['folder_cands_id'],
+    'null_tc': ['folder_cands_id','folder_id']
+}
+bronze_user_searches_dict = {
+    'duplicate_tc': ['user_searches_id'],
+    'null_tc': ['user_searches_id','users_id']
+}
+bronze_folder_candidates_status_log_dict = {
+    'duplicate_tc': ['folder_cands_status_log_id'],
+    'null_tc': ['folder_cands_status_log_id','candidate_id']
+}
--- scripts/bronze/bronze_layer_tc.py
+++ scripts/bronze/bronze_layer_tc.py
@@ -7,6 +7,10 @@ env = dbutils.widgets.get("Environment")
 app_id = dbutils.widgets.get("Client")
 tb_list = dbutils.widgets.get("TargetTableName")
 
+# env = "cert"
+# app_id = "xcloud"
+# tb_list = "bronze_candidate,bronze_folder,bronze_user_logins,bronze_users,bronze_folder_candidates,bronze_user_searches,bronze_folder_candidates_status_log"
+
 if(len(tb_list) == 0):
   trg_table_list = getTargetTableList(app_id)
 else:
@@ -16,6 +20,7 @@ suiteStartTime = getCurrentTime()
 
 for trg_tb in trg_table_list:
   
+  # verifying table BRONZE_CANDIDATE
   if (trg_tb == 'bronze_candidate'):
     for trg_col in getDuplicateTestTargetColumns(trg_tb):
       verify_duplicate_records(env, app_id, trg_tb,trg_col,suiteStartTime)
@@ -25,8 +30,8 @@ for trg_tb in trg_table_list:
 
     Verify_full_load_row_count(env,app_id,trg_tb,suiteStartTime)
 
+  # verifying table BRONZE_FOLDER
   if(trg_tb == 'bronze_folder'):
-    print("Running test for bronze_folder")
     for trg_col in getDuplicateTestTargetColumns(trg_tb):
       verify_duplicate_records(env, app_id, trg_tb,trg_col,suiteStartTime)
 
@@ -34,6 +39,57 @@ for trg_tb in trg_table_list:
       verify_null_records(env, app_id, trg_tb,trg_col,suiteStartTime)
     
     Verify_full_load_row_count(env,app_id,trg_tb,suiteStartTime)
+
+  # verifying table BRONZE_USER_LOGINS
+  if(trg_tb == 'bronze_user_logins'):
+    for trg_col in getDuplicateTestTargetColumns(trg_tb):
+      verify_duplicate_records(env, app_id, trg_tb,trg_col,suiteStartTime)
+
+    for trg_col in getNullTestTargetColumns(trg_tb):  
+      verify_null_records(env, app_id, trg_tb,trg_col,suiteStartTime)
+
+    Verify_full_load_row_count(env,app_id,trg_tb,suiteStartTime)
+
+  # verifying table BRONZE_USERS
+  if(trg_tb == 'bronze_users'):
+    for trg_col in getDuplicateTestTargetColumns(trg_tb):
+      verify_duplicate_records(env, app_id, trg_tb,trg_col,suiteStartTime)
+
+    for trg_col in getNullTestTargetColumns(trg_tb):  
+      verify_null_records(env, app_id, trg_tb,trg_col,suiteStartTime)
+    
+    Verify_full_load_row_count(env,app_id,trg_tb,suiteStartTime)
+
+  # verifying table BRONZE_FOLDER_CANDIDATE
+  if(trg_tb == 'bronze_folder_candidates'):
+    for trg_col in getDuplicateTestTargetColumns(trg_tb):
+      verify_duplicate_records(env, app_id, trg_tb,trg_col,suiteStartTime)
+
+    for trg_col in getNullTestTargetColumns(trg_tb):  
+      verify_null_records(env, app_id, trg_tb,trg_col,suiteStartTime)
+
+    Verify_full_load_row_count(env,app_id,trg_tb,suiteStartTime)
+
+  # verifying table BRONZE_USER_SEARCHES
+  if(trg_tb == 'bronze_user_searches'):
+    for trg_col in getDuplicateTestTargetColumns(trg_tb):
+      verify_duplicate_records(env, app_id, trg_tb,trg_col,suiteStartTime)
+
+    for trg_col in getNullTestTargetColumns(trg_tb):  
+      verify_null_records(env, app_id, trg_tb,trg_col,suiteStartTime)
+
+    Verify_full_load_row_count(env,app_id,trg_tb,suiteStartTime)
+
+  # verifying table BRONZE_FOLDER_CANDIDATE_STATUS_LOG
+  if(trg_tb == 'bronze_folder_candidates_status_log'):
+    for trg_col in getDuplicateTestTargetColumns(trg_tb):
+      verify_duplicate_records(env, app_id, trg_tb,trg_col,suiteStartTime)
+
+    for trg_col in getNullTestTargetColumns(trg_tb):  
+      verify_null_records(env, app_id, trg_tb,trg_col,suiteStartTime)
+
+    Verify_full_load_row_count(env,app_id,trg_tb,suiteStartTime)
+    
 
  
 
